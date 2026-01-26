a21e9afd1bbc35dd67ffb650a756a1333058d173
VishnuVardhan8055 <polankivishnuvardhan27@gmail.com>
2026-01-26 17:07:42 +0530

    Commit

---
+++ Python_2026/Data-Types.py
@@ -0,0 +1,18 @@
+"Data is different type like it's in Int, char, Float, Double, Binary, string, None, complex . These are basics. These are defeind the data in which type"
+
+" For to defined the in sequence we use List, tuple, range, dict, Boolean, set."
+
+A = 'A'
+B = "Hello"
+C = 8
+D = 0b10101010
+E = 24.50
+F = "HX+34J"
+G = True
+print(f" Type of A  : {type(A)}")
+print(f" Type of B  : {type(B)}")
+print(f" Type of C  : {type(C)}")
+print(f" Type of D  : {type(D)}")
+print(f" Type of E  : {type(E)}")
+print(f" Type of F  : {type(F)}")
+print(f" Type of G  : {type(G)}")
