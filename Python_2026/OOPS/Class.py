9bb55d40b12efc4646f56fc7c86ffb329f03d1a4
VishnuVardhan8055 <polankivishnuvardhan27@gmail.com>
2026-02-09 08:40:04 +0530

    OOPS

--- Python_2026/OOPS/Class.py
+++ Python_2026/OOPS/Class.py
@@ -5,10 +5,11 @@ class OOPS:
         self.name = name
         self.age = age
 
+
     def details(self):
         return f"{self.name}, {self.age} "
 
 name = "vishnu vardhan polanki"
 age = 23
-OBJ = OOPS(name, age)
-print("Here I am calling the class itself i will call the function, And it is Constructor"," \n",OBJ.details())
+Obj = OOPS(name, age)
+print("Here I am calling the class function which returns the values","\n",Obj.details())
