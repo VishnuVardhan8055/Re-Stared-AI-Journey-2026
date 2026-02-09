9b2a5ff711fe325dca21789332690fb2ffba526b
VishnuVardhan8055 <polankivishnuvardhan27@gmail.com>
2026-02-09 08:43:37 +0530

    OOPS

--- Python_2026/OOPS/Class.py
+++ Python_2026/OOPS/Class.py
@@ -13,3 +13,6 @@ name = "vishnu vardhan polanki"
 age = 23
 Obj = OOPS(name, age)
 print("Here I am calling the class function which returns the values","\n",Obj.details())
+
+""" Object is an Instance of a Class """
+"Here object we defined Obj it is instance of an OOPS class."
